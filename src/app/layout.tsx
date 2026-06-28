9a5b3a2d86d5f631f0acfaad99ef4b7305b43fd4
Joumana <jelmassouab@gmail.com>
2026-06-28 08:24:05 +0100

    Fix Favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -89,9 +89,12 @@ export const metadata: Metadata = {
   },
   manifest: '/manifest.json',
   icons: {
-    icon: '/favicon.ico',
-    shortcut: '/favicon-16x16.png',
-    apple: '/apple-touch-icon.png',
+    icon: [
+      { url: '/favicon.ico' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: { url: '/icons/icon-192.png', sizes: '192x192', type: 'image/png' },
   },
 }
 
