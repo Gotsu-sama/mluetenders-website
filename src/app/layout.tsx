81f900268060227a1e9b27528f68ebca4c09843e
Joumana <jelmassouab@gmail.com>
2026-06-28 08:02:20 +0100

    Fix Google Analytics event tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -95,7 +95,7 @@ export const metadata: Metadata = {
   },
 }
 
-const appSchema  = {
+const appSchema = {
   '@context': 'https://schema.org',
   '@type': 'SoftwareApplication',
   name: 'Mlue Tenders',
@@ -155,11 +155,18 @@ export default function RootLayout({
 
         <Script id="google-analytics" strategy="afterInteractive">
           {`
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'G-6PGJMKBZSW');
-          `}
+    window.dataLayer = window.dataLayer || [];
+
+    window.gtag = function() {
+      window.dataLayer.push(arguments);
+    };
+
+    window.gtag('js', new Date());
+
+    window.gtag('config', 'G-6PGJMKBZSW', {
+      debug_mode: true
+    });
+  `}
         </Script>
       </body>
     </html>
